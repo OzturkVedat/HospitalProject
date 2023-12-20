206f7855d523115120536934ba0cd97009dee9f6
OzturkVedat <92203253+OzturkVedat@users.noreply.github.com>
2023-12-20 22:08:24 +0300

    Delete HospitalProject directory
    branching errors

--- HospitalProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HospitalProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
