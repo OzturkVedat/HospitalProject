ab2310b51929ca7a4ab908cd1da13300e701c274
Emre Kara <emre.kara11@ogr.sakarya.edu.tr>
2023-12-28 16:51:57 +0300

    Doctor API Controller added

---
+++ HospitalProject/Controllers/DoctorAPIController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HospitalProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using static System.Collections.Specialized.BitVector32;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HospitalProject.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/doctor")]
+    [ApiController]
+    public class DoctorAPIController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+        public DoctorAPIController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<DoctorAPIController>
+        [HttpGet]
+        public IEnumerable<Doctor> Get()
+        {
+            return _context.Doctors.ToList();
+        }
+
+        // GET api/<DoctorAPIController>/5
+        [HttpGet("{id}")]
+        public Doctor Get(int id)
+        {
+            return _context.Doctors.FirstOrDefault(item => item.DoctorId == id);
+        }
+
+    }
+}
